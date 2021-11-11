dcc3c125622b3b13afdae92d5ed24484eadd47c1
Aqshola <aqsholafid2@gmail.com>
2021-11-11 21:54:40 +0700

    remove bundler analyzer

--- webpack.common.js
+++ webpack.common.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const { InjectManifest } = require("workbox-webpack-plugin");
 const ServiceWorkerWebpackPlugin = require("serviceworker-webpack-plugin");
-const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
+
 const ImageminWebpackPlugin = require("imagemin-webpack-plugin").default;
 const ImageminMozjpeg = require("imagemin-mozjpeg");
 
@@ -53,29 +53,28 @@ module.exports = {
         }),
       ],
     }),
-    new BundleAnalyzerPlugin(),
   ],
   optimization: {
-    // splitChunks: {
-    //   chunks: "all",
-    //   minSize: 20000,
-    //   maxSize: 70000,
-    //   minChunks: 1,
-    //   maxAsyncRequests: 30,
-    //   maxInitialRequests: 30,
-    //   automaticNameDelimiter: "~",
-    //   enforceSizeThreshold: 50000,
-    //   cacheGroups: {
-    //     defaultVendors: {
-    //       test: /[\\/]node_modules[\\/]/,
-    //       priority: -10,
-    //     },
-    //     default: {
-    //       minChunks: 2,
-    //       priority: -20,
-    //       reuseExistingChunk: true,
-    //     },
-    //   },
-    // },
+    splitChunks: {
+      chunks: "all",
+      minSize: 20000,
+      maxSize: 70000,
+      minChunks: 1,
+      maxAsyncRequests: 30,
+      maxInitialRequests: 30,
+      automaticNameDelimiter: "~",
+      enforceSizeThreshold: 50000,
+      cacheGroups: {
+        defaultVendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10,
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true,
+        },
+      },
+    },
   },
 };
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,11 +1,13 @@
-const { merge } = require('webpack-merge')
-const path = require('path')
-const common = require('./webpack.common')
+const { merge } = require("webpack-merge");
+const path = require("path");
+const common = require("./webpack.common");
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
 
 module.exports = merge(common, {
-  mode: 'development',
+  mode: "development",
+  plugins: [new BundleAnalyzerPlugin()],
   devServer: {
     open: true,
-    contentBase: path.resolve(__dirname, 'dist'),
+    contentBase: path.resolve(__dirname, "dist"),
   },
-})
+});
