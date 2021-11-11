68eb5c9e5e9f804b50c9ece9ac3328c7cf6b7e3d
Aqshola <aqsholafid2@gmail.com>
2021-11-11 21:36:52 +0700

    removeoptimization

--- webpack.common.js
+++ webpack.common.js
@@ -56,26 +56,26 @@ module.exports = {
     new BundleAnalyzerPlugin(),
   ],
   optimization: {
-    splitChunks: {
-      chunks: "all",
-      minSize: 20000,
-      maxSize: 70000,
-      minChunks: 1,
-      maxAsyncRequests: 30,
-      maxInitialRequests: 30,
-      automaticNameDelimiter: "~",
-      enforceSizeThreshold: 50000,
-      cacheGroups: {
-        defaultVendors: {
-          test: /[\\/]node_modules[\\/]/,
-          priority: -10,
-        },
-        default: {
-          minChunks: 2,
-          priority: -20,
-          reuseExistingChunk: true,
-        },
-      },
-    },
+    // splitChunks: {
+    //   chunks: "all",
+    //   minSize: 20000,
+    //   maxSize: 70000,
+    //   minChunks: 1,
+    //   maxAsyncRequests: 30,
+    //   maxInitialRequests: 30,
+    //   automaticNameDelimiter: "~",
+    //   enforceSizeThreshold: 50000,
+    //   cacheGroups: {
+    //     defaultVendors: {
+    //       test: /[\\/]node_modules[\\/]/,
+    //       priority: -10,
+    //     },
+    //     default: {
+    //       minChunks: 2,
+    //       priority: -20,
+    //       reuseExistingChunk: true,
+    //     },
+    //   },
+    // },
   },
 };
