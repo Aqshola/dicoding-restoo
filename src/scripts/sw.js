91d4d76095bcc6680b447ee0bf45bc8de63b788b
Aqshola <aqsholafid2@gmail.com>
2021-11-11 22:48:17 +0700

    fixing sw

--- src/scripts/sw.js
+++ src/scripts/sw.js
@@ -1,32 +1,26 @@
 /* eslint-disable no-underscore-dangle */
 /* eslint-disable no-restricted-globals */
-import { cleanupOutdatedCaches, precacheAndRoute } from 'workbox-precaching'
-import { registerRoute } from 'workbox-routing'
-import { CacheFirst, NetworkFirst } from 'workbox-strategies'
-import { ExpirationPlugin } from 'workbox-expiration'
-import { clientsClaim, setCacheNameDetails } from 'workbox-core'
+import "regenerator-runtime/runtime";
+import { cleanupOutdatedCaches, precacheAndRoute } from "workbox-precaching";
+import { registerRoute } from "workbox-routing";
+import { CacheFirst, NetworkFirst } from "workbox-strategies";
+import { ExpirationPlugin } from "workbox-expiration";
+import { clientsClaim, setCacheNameDetails } from "workbox-core";
 
-self.addEventListener('install', () => {
-  console.log('Installing Service Worker')
-})
-self.addEventListener('activate', () => {
-  console.log('Activate')
-})
-
-self.skipWaiting()
-clientsClaim()
+self.skipWaiting();
+clientsClaim();
 
 setCacheNameDetails({
-  prefix: 'Restoo',
-  precache: 'precache',
-  runtime: 'runtime',
-})
+  prefix: "Restoo",
+  precache: "precache",
+  runtime: "runtime",
+});
 
 precacheAndRoute(
   [
     ...self.__WB_MANIFEST,
     {
-      url: 'https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap',
+      url: "https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap",
       revision: 1,
     },
   ],
@@ -34,13 +28,13 @@ precacheAndRoute(
   {
     ignoreURLParametersMatching: [/.*/],
   }
-)
+);
 
 registerRoute(
   /^https:\/\/restaurant-api.dicoding.dev\/(?:(list|detail))/,
 
   new NetworkFirst({
-    cacheName: 'dicoding-restaurant-api',
+    cacheName: "dicoding-restaurant-api",
     plugins: [
       new ExpirationPlugin({
         maxAgeSeconds: 60 * 60 * 24 * 30 * 2,
@@ -48,13 +42,13 @@ registerRoute(
       }),
     ],
   }),
-  'GET'
-)
+  "GET"
+);
 
 registerRoute(
-  ({ request }) => request.destination === 'image',
+  ({ request }) => request.destination === "image",
   new CacheFirst({
-    cacheName: 'images',
+    cacheName: "images",
     plugins: [
       new ExpirationPlugin({
         maxEntries: 60,
@@ -62,6 +56,6 @@ registerRoute(
       }),
     ],
   })
-)
+);
 
-cleanupOutdatedCaches()
+cleanupOutdatedCaches();
--- webpack.common.js
+++ webpack.common.js
@@ -2,7 +2,7 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const { InjectManifest } = require("workbox-webpack-plugin");
-const ServiceWorkerWebpackPlugin = require("serviceworker-webpack-plugin");
+// const ServiceWorkerWebpackPlugin = require("serviceworker-webpack-plugin");
 
 const ImageminWebpackPlugin = require("imagemin-webpack-plugin").default;
 const ImageminMozjpeg = require("imagemin-mozjpeg");
@@ -28,9 +28,9 @@ module.exports = {
       template: path.resolve(__dirname, "src/templates/index.html"),
       filename: "index.html",
     }),
-    new ServiceWorkerWebpackPlugin({
-      entry: path.resolve(__dirname, "src/scripts/sw.js"),
-    }),
+    // new ServiceWorkerWebpackPlugin({
+    //   entry: path.resolve(__dirname, "src/scripts/sw.js"),
+    // }),
     new CopyWebpackPlugin({
       patterns: [
         {
