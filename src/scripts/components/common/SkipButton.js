4539fa409b3483f19238c4fbcc45fc73e5af52a7
Aqshola <aqsholafid2@gmail.com>
2021-11-18 13:13:30 +0700

    fix skip btn

--- src/scripts/components/common/SkipButton.js
+++ src/scripts/components/common/SkipButton.js
@@ -1,28 +1,41 @@
 /* eslint-disable no-useless-constructor */
 class SkipButton extends HTMLElement {
   constructor() {
-    super()
+    super();
   }
 
   connectedCallback() {
-    this.btn = ''
-    this.render()
-    this.afterRender()
+    this.btn = "";
+    this.render();
+    this.afterRender();
   }
 
   render() {
     this.innerHTML = `
-    <a href="#main-content" class="skip-btn">Skip to Content</a>
-    `
+    <button tabindex="0" class="skip-btn">Skip to Content</button>
+    `;
   }
 
   afterRender() {
-    this.btn = document.querySelector('.skip-btn')
+    this.btn = document.querySelector(".skip-btn");
 
-    this.btn.addEventListener('click', () => {
-      document.getElementById('main-content').scrollIntoView()
-    })
+    this.btn.addEventListener("click", (e) => {
+      e.preventDefault();
+      document.getElementById("main-content").scrollIntoView();
+      document.getElementById("main-content").focus();
+
+      const card = document.querySelector(".card .card-description a");
+      const favBtn = document.querySelector("#fav-btn");
+
+      if (card !== null) {
+        card.focus();
+      }
+
+      if (favBtn) {
+        favBtn.focus();
+      }
+    });
   }
 }
 
-export default customElements.define('skip-button', SkipButton)
+export default customElements.define("skip-button", SkipButton);
